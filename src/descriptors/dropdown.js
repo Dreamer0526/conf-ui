1ed28d3f6da05a3b2c0f6ed5b384853773977430
Mengzhu <xiemengzhu0526@gmail.com>
2019-04-11 10:48:23 +0800

    add field example for dropdown

---
+++ src/descriptors/dropdown.js
@@ -0,0 +1,22 @@
+import { DROPDOWN } from "../../../constants/componentTypes";
+import { TEST } from "../constants/actionTypes";
+
+export const dropdown_language = {
+  type: DROPDOWN,
+  width: 3,
+  title: "Default title",
+  defaultSelectedKey: "key1",
+  events: {
+    onClick: TEST
+  },
+  options: [
+    {
+      textId: "option1",
+      key: "key1",
+      selected: true
+    }, {
+      textId: "option2",
+      key: "key2",
+    }
+  ]
+};
